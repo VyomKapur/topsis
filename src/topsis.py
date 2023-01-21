840e97939eaf6b0a06c3be2ef8e940ef86c52498
Vyom Kapur <vkapur_be20@thapar.edu>
2023-01-21 09:38:42 +0530

    first commit

---
+++ src/topsis.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+result_file = input("Enter output file: ")
+data_file = input("Enter csv data file: ")
+
+data = pd.read_csv(data_file, header=None)
+print(data)
+
+#apply vector normalization
+
+
+
+#multiply weights
+
